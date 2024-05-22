58e702c58a922c01af6fb634e022e5013d4a2358
Facundo HM <facundo.hmz@gmail.com>
2024-05-21 23:49:35 -0300

    Update data processing code

--- data-processing.py
+++ data-processing.py
@@ -1,19 +1,21 @@
-from sklearn import datasets, model_selection
+from sklearn import datasets, model_selection, preprocessing, metrics
 import numpy as np
 import pandas as pd
 
 housing = datasets.fetch_california_housing(as_frame=True)
 housing_data: pd.DataFrame = housing['data']
-housing_target: pd.DataFrame = housing['target']
+housing_target: pd.Series = housing['target']
 housing_total_data: pd.DataFrame = pd.concat(
     [housing_data, housing_target], axis=1)
 
 housing_total_data.info()
-print(housing_data.describe())
-
 print('housing_total_data', housing_total_data)
-print('housing_data', housing_data)
-print('housing_target', housing_target)
+
+# Scale data and target values
+data_scaler = preprocessing.StandardScaler()
+target_scaler = preprocessing.StandardScaler()
+housing_data_scaled = data_scaler.fit_transform(housing_data)
+housing_target_scaled = target_scaler.fit_transform(housing_target.to_frame())
 
 housing_data['IncomeCat'] = pd.cut(
     housing_data['MedInc'],
@@ -34,3 +36,11 @@ for X_set in (X_train, X_test):
 # attribute and MedHouseVal
 corr_matrix = housing_total_data.corr()
 print(corr_matrix['MedHouseVal'].sort_values(ascending=False))
+
+# Use custom transformer to create a feature that measure
+# the geographic similarity between each district and San Francisco
+sf_coords = 37.7749, -122.41
+sf_transformer = preprocessing.FunctionTransformer(
+    metrics.pairwise.rbf_kernel,
+    kw_args=dict(Y=[sf_coords], gamma=0.1))
+sf_simil = sf_transformer.transform(housing_data[['Latitude', 'Longitude']])
