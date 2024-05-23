a44a27b45cfa18f3d013ca4a53dc4475915071a1
Facundo HM <facundo.hmz@gmail.com>
2024-05-22 23:56:26 -0300

    Update data processing code

--- data-processing.py
+++ data-processing.py
@@ -1,40 +1,37 @@
-from sklearn import datasets, model_selection, preprocessing, metrics
+from sklearn import (
+    datasets, model_selection, preprocessing,
+    metrics, base, cluster)
 import numpy as np
 import pandas as pd
 
 housing = datasets.fetch_california_housing(as_frame=True)
-housing_data: pd.DataFrame = housing['data']
-housing_target: pd.Series = housing['target']
-housing_total_data: pd.DataFrame = pd.concat(
-    [housing_data, housing_target], axis=1)
+X: pd.DataFrame = housing['data']
+y: pd.Series = housing['target']
+housing_X_y: pd.DataFrame = pd.concat([X, y], axis=1)
 
-housing_total_data.info()
-print('housing_total_data', housing_total_data)
+housing_X_y.info()
+print('housing_X_y', housing_X_y)
 
 # Scale data and target values
 data_scaler = preprocessing.StandardScaler()
 target_scaler = preprocessing.StandardScaler()
-housing_data_scaled = data_scaler.fit_transform(housing_data)
-housing_target_scaled = target_scaler.fit_transform(housing_target.to_frame())
+X_scaled = data_scaler.fit_transform(X)
+y_scaled = target_scaler.fit_transform(y.to_frame())
 
-housing_data['IncomeCat'] = pd.cut(
-    housing_data['MedInc'],
+X['IncomeCat'] = pd.cut(
+    X['MedInc'],
     bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
     labels=[1, 2, 3, 4, 5])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
-    housing_data,
-    housing_target,
-    test_size=0.2,
-    stratify=housing_data['IncomeCat'],
-    random_state=42)
+    X, y, test_size=0.2, stratify=X['IncomeCat'], random_state=42)
 
 for X_set in (X_train, X_test):
     X_set.drop('IncomeCat', axis=1, inplace=True)
 
 # Compute the standard correlation coefficient between each
 # attribute and MedHouseVal
-corr_matrix = housing_total_data.corr()
+corr_matrix = housing_X_y.corr()
 print(corr_matrix['MedHouseVal'].sort_values(ascending=False))
 
 # Use custom transformer to create a feature that measure
@@ -43,4 +40,40 @@ sf_coords = 37.7749, -122.41
 sf_transformer = preprocessing.FunctionTransformer(
     metrics.pairwise.rbf_kernel,
     kw_args=dict(Y=[sf_coords], gamma=0.1))
-sf_simil = sf_transformer.transform(housing_data[['Latitude', 'Longitude']])
+sf_simil: np.ndarray = sf_transformer.transform(
+    X[['Latitude', 'Longitude']])
+
+# Custom class transformer.
+# Use KMeans clusterer in the fit() method to identify the main
+# clusters in the training data.
+# Use rbf_kernel() in thetransform() method to measure how similar
+# each sample is to each cluster center.
+class ClusterSimilarity(base.BaseEstimator, base.TransformerMixin):
+    def __init__(
+        self, n_clusters=10, gamma=1.0, random_state: int = None
+    ):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(
+        self, X: np.ndarray, y=None, sample_weight: np.ndarray = None
+    ):
+        # Locate the clusters
+        self.kmeans_ = cluster.KMeans(
+            self.n_clusters, random_state=self.random_state)
+        # Weight each district by its median house value
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+
+        return self
+
+    def transform(self, X: np.ndarray):
+        # Measure the Gaussian RBF similarity between each district
+        # and all 10 cluster centers
+        return metrics.pairwise.rbf_kernel(
+            X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+
+cluster_simil = ClusterSimilarity(
+    n_clusters=10, gamma=1., random_state=42)
+similarities = cluster_simil.fit_transform(
+    X[['Latitude', 'Longitude']], sample_weight=y)
