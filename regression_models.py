6080a847fc6d7d9e0c60eddeca0cfcffaf0061bc
Facundo HM <facundo.hmz@gmail.com>
2024-06-02 23:27:58 -0300

    Update regression models code

--- regression_models.py
+++ regression_models.py
@@ -1,42 +1,43 @@
-from sklearn import (
-    linear_model, pipeline, metrics, tree, model_selection, ensemble)
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.metrics import root_mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import randint
 from data_processing import (
     X_train, y_train, X_test, y_test, columns_transformer)
 
-lin_reg = pipeline.make_pipeline(
-    columns_transformer, linear_model.LinearRegression())
+lin_reg = make_pipeline(columns_transformer, LinearRegression())
 lin_reg.fit(X_train, y_train)
 
 lin_reg_predictions = lin_reg.predict(X_train)
-lin_reg_rmse = metrics.root_mean_squared_error(
-    y_train, lin_reg_predictions)
+lin_reg_rmse = root_mean_squared_error(y_train, lin_reg_predictions)
 print('lin_reg_predictions', lin_reg_predictions[:5])
 print('lin_reg_labels', y_train.iloc[:5].values)
 print('lin_reg_rmse', lin_reg_rmse)
 
-tree_reg = pipeline.make_pipeline(
+tree_reg = make_pipeline(
     columns_transformer,
-    tree.DecisionTreeRegressor(random_state=42))
+    DecisionTreeRegressor(random_state=42))
 tree_reg.fit(X_train, y_train)
 
 tree_reg_predictions = tree_reg.predict(X_train)
-tree_reg_rmse = metrics.root_mean_squared_error(
-    y_train, tree_reg_predictions)
+tree_reg_rmse = root_mean_squared_error(y_train, tree_reg_predictions)
 print('tree_reg_predictions', tree_reg_predictions[:5])
 print('tree_reg_labels', y_train.iloc[:5].values)
 print('tree_reg_rmse', tree_reg_rmse)
 
-tree_reg_cross_val = pipeline.make_pipeline(
+tree_reg_cross_val = make_pipeline(
     columns_transformer,
-    tree.DecisionTreeRegressor(random_state=42))
+    DecisionTreeRegressor(random_state=42))
 # Randomly split the training set into 10 nonoverlapping
 # subsets called folds.
 # Train and evaluate the decision tree model 10 times.
 # Pick a different fold for evaluation every time and use
 # the other 9 folds for training.
 # Return an array containing the 10 evaluation scores.
-tree_rmses = model_selection.cross_val_score(
+tree_rmses = cross_val_score(
     tree_reg_cross_val,
     X_train,
     y_train,
@@ -44,13 +45,12 @@ tree_rmses = model_selection.cross_val_score(
     cv=10)
 print('tree_rmses', tree_rmses)
 
-forest_reg_pipeline = pipeline.Pipeline([
+forest_reg_pipeline = Pipeline([
     ('columns_transformer', columns_transformer),
-    ('random_forest', ensemble.RandomForestRegressor(
-        random_state=42))
+    ('random_forest', RandomForestRegressor(random_state=42))
 ])
 
-forest_rmses = model_selection.cross_val_score(
+forest_rmses = cross_val_score(
     forest_reg_pipeline,
     X_train,
     y_train,
@@ -62,7 +62,7 @@ rnd_param = {
     'columns_transformer__geo__n_clusters': randint(low=3, high=50),
     'random_forest__max_features': randint(low=2, high=20)}
 
-rnd_search = model_selection.RandomizedSearchCV(
+rnd_search = RandomizedSearchCV(
     forest_reg_pipeline,
     param_distributions=rnd_param,
     n_iter=10,
@@ -75,6 +75,6 @@ print('rnd_search.best_params_', rnd_search.best_params_)
 forest_reg_model = rnd_search.best_estimator_
 forest_reg_predictions = forest_reg_model.predict(X_test)
 
-forest_reg_rmse = metrics.root_mean_squared_error(
+forest_reg_rmse = root_mean_squared_error(
     y_test, forest_reg_predictions, squared=False)
 print('forest_reg_rmse', forest_reg_rmse)
