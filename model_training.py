e36e9ac37d26248b76aeb2dc37140126e75855ff
Facundo HM <facundo.hmz@gmail.com>
2024-06-13 23:47:42 -0300

    Update model training code

--- model_training.py
+++ model_training.py
@@ -28,7 +28,6 @@ print('y_predict', y_predict)
 theta_hat_pinv = np.linalg.pinv(X) @ y
 print('pseudoinverse', theta_hat_pinv)
 
-
 # Batch Gradient Descent
 # learning rate
 eta = 0.1
@@ -37,7 +36,27 @@ n_epochs = 1000
 theta = np.random.randn(2, 1)
 
 for _ in range(n_epochs):
+    # Compute the gradient vector of the MSE cost function
     gradients = 2 / m * X.T @ (X @ theta - y)
     theta = theta - eta * gradients
+
+# Stochastic Gradient Descent
+n_epochs = 50
+# Learning schedule hyperparameters
+t0, t1 = 5, 50
+# Determine the learning rate
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2, 1)
+
+for epoch in range(n_epochs):
+    for iteration in range(m):
+        random_index = np.random.randint(m)
+        xi = X[random_index : random_index + 1]
+        yi = y[random_index : random_index + 1]
+        gradients = 2 * xi.T @ (xi @ theta - yi)
+        eta = learning_schedule(epoch * m + iteration)
+        theta = theta - eta * gradients
 
 print('theta', theta)
