e60b7b0133605795694f94d3f3c337042fbd85a9
Facundo HM <facundo.hmz@gmail.com>
2024-06-14 23:20:07 -0300

    Update model training code

--- model_training.py
+++ model_training.py
@@ -1,4 +1,5 @@
-from sklearn.preprocessing import add_dummy_feature
+from sklearn.preprocessing import add_dummy_feature, PolynomialFeatures
+from sklearn.linear_model import SGDRegressor, LinearRegression
 import numpy as np
 
 # Normal equation
@@ -7,11 +8,11 @@ import numpy as np
 np.random.seed(42)
 # number of instances
 m = 100
-X = 2 * np.random.rand(m, 1)
+X_init = 2 * np.random.rand(m, 1)
 # y = 4 + 3x₁ + Gaussian noise
-y = 4 + 3 * X + np.random.randn(m, 1)
+y = 4 + 3 * X_init + np.random.randn(m, 1)
 # Add x0 = 1 to each instance
-X = add_dummy_feature(X)
+X = add_dummy_feature(X_init)
 # Compute the inverse of a matrix and perform matrix multiplication
 theta_hat = np.linalg.inv(X.T @ X) @ X.T @ y
 print('theta_hat', theta_hat)
@@ -60,3 +61,27 @@ for epoch in range(n_epochs):
         theta = theta - eta * gradients
 
 print('theta', theta)
+
+# Perform linear regression using stochastic GD
+sgd = SGDRegressor(
+    max_iter=1000,
+    tol=1e-5,
+    penalty=None,
+    eta0=0.01,
+    n_iter_no_change=100,
+    random_state=42)
+sgd.fit(X_init, y.ravel())
+print('Bias term: ', sgd.intercept_, ', Weights: ', sgd.coef_)
+
+# Polynomial Regression
+X = 6 * np.random.rand(m, 1) - 3
+# Simple quadratic equation, y = ax² + bx + c plus some noise
+y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+# Add the square of each feature as a new feature
+X_poly = poly_features.fit_transform(X)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+print('Bias term: ', lin_reg.intercept_, ', Weights: ', lin_reg.coef_)
