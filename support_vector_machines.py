9b51435afdaae8df6b01b3dba0046cc6819e03ff
Facundo HM <facundo.hmz@gmail.com>
2024-06-26 23:06:51 -0300

    Update support vector machines code

--- support_vector_machines.py
+++ support_vector_machines.py
@@ -2,9 +2,10 @@ from typing import cast
 from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import LinearSVC, SVC, LinearSVR
 from sklearn.datasets import make_moons
 from pandas import DataFrame
+import numpy as np
 
 iris = load_iris(as_frame=True)
 iris_data = cast(DataFrame, iris.data)
@@ -31,8 +32,6 @@ print('svm_scores', svm_scores)
 ### NONLINEAR SVM CLASSIFICATION ###
 # Create dataset for binary classification
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
-print('X_moons', X)
-print('X_moons_poly', PolynomialFeatures(degree=2, include_bias=False).fit_transform(X))
 
 svm_poly = make_pipeline(
     PolynomialFeatures(degree=3),
@@ -45,7 +44,28 @@ svm_poly.fit(X, y)
 # Implement the kernel trick which gets the same result as adding
 # many polynomial features, even with a very high degree,
 # without actually having to add them.
-poly_kernel_svm_clf = make_pipeline(
+poly_kernel_svc = make_pipeline(
     StandardScaler(),
     SVC(kernel='poly', degree=3, coef0=1, C=5))
-poly_kernel_svm_clf.fit(X, y)
+poly_kernel_svc.fit(X, y)
+
+### GAUSSIAN RBF KERNEL ###
+# Implement the kernel trick which gets the same result as adding
+# many similarity features, but without actually having to add them.
+rbf_kernel_svc = make_pipeline(
+    StandardScaler(),
+    SVC(kernel='rbf', gamma=5, C=0.001))
+rbf_kernel_svc.fit(X, y)
+
+### SVM REGRESSION ###
+# Try to fit as many instances as possible on the street while
+# limiting margin violations, instances off the street.
+np.random.seed(42)
+m = 50
+X_linear = 2 * np.random.rand(m, 1)
+y_linear = 4 + 3 * X_linear.ravel() + np.random.randn(m)
+
+svm_linear = make_pipeline(
+    StandardScaler(),
+    LinearSVR(epsilon=0.5, random_state=42))
+svm_linear.fit(X, y)
