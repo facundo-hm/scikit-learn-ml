94ae3aadf1706878ceff1a7df6883901826471aa
Facundo HM <facundo.hmz@gmail.com>
2024-07-12 23:50:00 -0300

    Update utils

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_mnist_digit(image_data: np.ndarray):
+CHARTS_PATH = './charts/'
+
+def plot_mnist_digit(image_data: np.ndarray, image_name: str):
     image = image_data.reshape(28, 28)
     plt.imshow(image, cmap='binary')
     plt.axis('off')
+    plt.savefig(CHARTS_PATH + image_name)
+
+def save_representative_imgs(representative_imgs: np.ndarray, k:int):
+    for index, X_representative_digit in enumerate(representative_imgs):
+        plt.subplot(k // 10, 10, index + 1)
+        plt.imshow(
+            X_representative_digit.reshape(8, 8),
+            cmap='binary', interpolation='bilinear')
+        plt.axis('off')
+
+    plt.savefig(CHARTS_PATH + 'representative_images_plot')
