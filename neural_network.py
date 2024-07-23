4939b5c584523768829c253b2ec0c11f3ca86fa9
Facundo HM <facundo.hmz@gmail.com>
2024-07-22 22:48:21 -0300

    Create neural network file

---
+++ neural_network.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris(as_frame=True)
+
+X = iris.data[['petal length (cm)', 'petal width (cm)']].values
+y = (iris.target == 0)
+
+per_clf = Perceptron(random_state=42)
+per_clf.fit(X, y)
+
+X_test = [[2, 0.5], [3, 1]]
+pred = per_clf.predict(X_test)
