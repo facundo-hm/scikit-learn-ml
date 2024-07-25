74495a7355999d9f02c8473d67238c2d79fe5f08
Facundo HM <facundo.hmz@gmail.com>
2024-07-24 22:47:31 -0300

    Update neural network code

--- neural_network.py
+++ neural_network.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_iris, fetch_california_housing
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 import numpy as np
@@ -11,17 +11,17 @@ from pandas import DataFrame
 
 iris = load_iris(as_frame=True)
 
-X = iris.data[['petal length (cm)', 'petal width (cm)']].values
-y = (iris.target == 0)
+X_iris = iris.data[['petal length (cm)', 'petal width (cm)']].values
+y_iris = (iris.target == 0)
 
-X_train, X_test, y_train, y_test = cast(
+X_iris_train, X_iris_test, y_iris_train, y_iris_test = cast(
     list[DataFrame],
-    train_test_split(X, y, test_size=0.2, random_state=42))
+    train_test_split(X_iris, y_iris, test_size=0.2, random_state=42))
 
 perceptron = Perceptron(random_state=42)
-perceptron.fit(X, y)
+perceptron.fit(X_iris_train, y_iris_train)
 
-perceptron.score(X_test, y_test)
+perceptron.score(X_iris_test, y_iris_test)
 
 ### REGRESSION MLP ###
 housing = fetch_california_housing()
@@ -39,3 +39,11 @@ pipeline.fit(X_train, y_train)
 
 y_pred = pipeline.predict(X_valid)
 root_mean_squared_error(y_valid, y_pred)
+
+### CLASSIFICATION MLP ###
+mlpc = MLPClassifier(
+    hidden_layer_sizes=[10], max_iter= 700, random_state=42)
+pipeline = make_pipeline(StandardScaler(), mlpc)
+pipeline.fit(X_iris_train, y_iris_train)
+
+mlpc.score(X_iris_test, y_iris_test)
